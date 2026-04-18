2972c1fcd969f14b52f05682a14d804b489ce429
Chenblue <Wanglemao03@163.com>
2026-04-18 17:11:59 +0800

    设置查询商铺状态信息是打样还是休息

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    /**
+     * 设置店铺状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    public Result setStatus(@PathVariable Integer status){
+
+        log.info("设置店铺状态为：{}" , status == 1 ? "营业中" : "打样中");
+        redisTemplate.opsForValue().set("SHOP_STATUS" , status);
+
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取到店铺的营业状态：{}" , shopStatus == 1 ? "营业中" : "打样中" );
+        return Result.success(shopStatus);
+    }
+}
