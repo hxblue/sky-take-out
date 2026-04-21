6b9bde9b3675aef4a2e7bfdd49c9ab59353840ec
Chenblue <Wanglemao03@163.com>
2026-04-21 22:10:50 +0800

    缓存套餐开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -8,6 +8,7 @@ import com.sky.service.SetmealService;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
@@ -27,6 +28,7 @@ public class SetmealController {
       * @return
       */
      @PostMapping
+     @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
      public Result save(@RequestBody SetmealDTO setmealDTO){
          setmealService.saveWithDish(setmealDTO);
          return Result.success();
@@ -49,6 +51,7 @@ public class SetmealController {
       * @return
       */
      @DeleteMapping
+     @CacheEvict(cacheNames = "setmealCache" , allEntries = true )
      public Result delete(@RequestParam List<Long> ids){
 
          setmealService.deleteBatch(ids);
@@ -62,12 +65,14 @@ public class SetmealController {
      }
 
      @PutMapping
+     @CacheEvict(cacheNames = "setmealCache" , allEntries = true )
      public Result update(@RequestBody SetmealDTO setmealDTO){
          setmealService.update(setmealDTO);
          return Result.success();
      }
 
      @PostMapping("/status/{status}")
+     @CacheEvict(cacheNames = "setmealCache" , allEntries = true )
      public Result startOrStop(@PathVariable Integer  status, Long id){
             setmealService.startOrStop(status, id);
             return Result.success();
