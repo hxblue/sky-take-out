7b0247ebd010572fea2071c1d5c080b281e996d9
Chenblue <Wanglemao03@163.com>
2026-04-21 23:11:58 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -77,4 +77,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
           }
 
     }
+
+    /**
+     * 显示购物车
+     * @return
+     */
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart =   ShoppingCart.builder()
+                        .userId(userId)
+                                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        return list;
+    }
 }
